200820fbcb29b571c13216fe647d76528edf9bd0
vikaxxh <stax.vkp086@gmail.com>
2023-08-15 14:05:52 +0530

    sec

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const app = require('./app');
-const { connectDatabase } = require('./config/database');
-const cloudinary = require("cloudinary")
+const app = require("./app");
+const { connectDatabase } = require("./config/database");
+const cloudinary = require("cloudinary");
 
 connectDatabase();
 cloudinary.config({
-    clound_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  clound_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
